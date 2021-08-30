d1026e6655d5877dd7ac06f7528a954edf2ec921
bryant <2399315737@qq.com>
2021-08-30 15:15:47 +0800

    Update struct.go

--- struct.go
+++ struct.go
@@ -20,4 +20,8 @@ func main() {
 
 	// 忽略的字段为 0 或 空
 	fmt.Println(Books{title: "Go 语言", author: "www.runoob.com"})
+	// 	1.结构体类似于PHP中的class\对象
+	// 	2.结构体与指针结合遇到 不用*就能直接获取结构体元素内容
+	//	3.结构体类型的函数能直接当成结构体方法使用（a.foo()） func (a A)foo int{}
+	//	4.结构体函数 可以用结构体指针（指针变量）函数内部改变结构体值func (a *A)foo int{}
 }
